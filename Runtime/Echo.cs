d2b23bf72c01131602ec0d6f5c4946e80041adb4
Martin Bo Laursen <martin.bo.laursen@lego.com>
2019-07-04 21:23:22 +0200

    Initial commit

---
+++ Runtime/Echo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace First.Runtime
+{
+    public static class Echo
+    {
+        public static string Now(string s)
+        {
+            var output = "First:" + s;
+            return output;
+        }
+    }
+}
